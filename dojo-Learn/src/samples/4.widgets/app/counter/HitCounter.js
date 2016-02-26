6ab232af309631a2369aaeb3988ef3656280ae36
nagabhushanam <nagabhushanamn@gmail.com>
2016-02-26 16:32:49 +0530

    dojo custom widgets

--- dojo-Learn/src/samples/4.widgets/app/counter/HitCounter.js
+++ dojo-Learn/src/samples/4.widgets/app/counter/HitCounter.js
@@ -2,58 +2,72 @@
  * http://usejsdoc.org/
  */
 
-define([ 'dojo',
+define([ 'dojo', 
          'dijit/_WidgetBase',
-         'dojo/dom-construct',
-         'dojo/on'],
-         
-function(dojo,_WidgetBase,domConstruct,on) {
+         'dijit/_TemplatedMixin',
+		 'dojo/dom-construct',
+		 'dojo/on',
+		 'dojo/text!./template/HitCounter.html' ],
 
-	var HitCounter = dojo.declare([_WidgetBase], {
-		
-		_count:0,
-		btn:null,
-		
-		// Init-phase
-		constructor:function(){
-			console.log('::constructor');
-		},
-		postMixInProperties:function(){
-			console.log('::postMixInProperties');
-			this._i=10;
-		},
-		
-		buildRendering:function(){
-			// construct UI
-			console.log('::buildRendering');
-			this.btn=domConstruct.create('button',{innerHTML:'Hit Me',class:'btn btn-primary'});
-			
-			this.domNode=this.btn;   // UI
-		},
-		postCreate:function(){
-			// final touch-up work to widget , before we place in page
-			// e.g binding event-listeners
-			console.log('::postCreate');
-			
-			this.own(
-					on(this.btn,'click',this.doCount.bind(this))		
-			);
-			
-		},
-		// Use-phase
-		doCount:function(){
-			console.log("::doCount");
-			this._count++;
-			//console.log('count:'+this._count);
-			this.btn.innerHTML=this._count;
-		},
-		// Destroy-phase
-		destroy:function(){
-			console.log('::destroy');
-			this.inherited(arguments);
-		}
-	});
+		function(dojo, _WidgetBase, _TemplatedMixin, domConstruct, on, template) {
 
-	return HitCounter;
+			var HitCounter = dojo.declare([ _WidgetBase, _TemplatedMixin ], {
 
-});
+				_count : 0,
+				// btn:null,
+				templateString : template,
+				// Init-phase
+				constructor : function() {
+					console.log('::constructor');
+				},
+				postMixInProperties : function() {
+					console.log('::postMixInProperties');
+				},
+				
+				// buildRendering:function(){
+				// // construct UI
+				// console.log('::buildRendering');
+				// this.btn=domConstruct.create('button',{innerHTML:'HitMe',class:'btn btn-primary'});
+				// this.domNode=this.btn; // UI
+				// },
+				
+				_setCountAttr:function(count){
+					console.log('::set count');
+					if(count){
+						this._count=count;
+						this.countSpan.innerHTML=this._count;
+					}
+				},
+				_getCountAttr:function(count){
+					console.log('::get count');
+					return this._count;
+				},
+				
+				postCreate : function() {
+					// final touch-up work to widget , before we place in page
+					// e.g binding event-listeners
+					console.log('::postCreate');
+					this.own(on(this.btn, 'click', this.doCount.bind(this)));
+
+				},
+				// Use-phase
+				doCount : function() {
+					console.log("::doCount");
+					this._count++;
+					// console.log('count:'+this._count);
+					// this.btn.innerHTML=this._count;
+					//this.countSpan.innerHTML = this._count;
+					this.set('count',this._count);
+				},
+				// Destroy-phase
+				destroy : function() {
+					console.log('::destroy');
+					this.inherited(arguments);
+				}
+				
+			});
+
+			return HitCounter;
+			
+
+		});
--- dojo-Learn/src/samples/4.widgets/app/main.js
+++ dojo-Learn/src/samples/4.widgets/app/main.js
@@ -4,16 +4,24 @@
 
 define([ './counter/HitCounter' ,'dojo/dom'], function(HitCounter,dom) {
 
-	var c1 = new HitCounter();
+	var c1 = new HitCounter({count:100});
 	c1.placeAt(dom.byId('counterBox'));
 	
 
-	// var c2 = new HitCounter();
-	// c2.placeAt(dom.byId('counterBox'));
+	 var c2 = new HitCounter();
+	 //c2.set('count',200);
+	 c2.placeAt(dom.byId('counterBox'));
 	
 
 	 setTimeout(function() {
-	 c1.destroy();
+		 console.log(c1.get('count'));
 	 }, 5000);
+	 
+	 var i=0;
+	 setInterval(function() {
+	 	i++;
+	 	c2.set('count',i);
+	 }, 1000);
 
+	 
 });
